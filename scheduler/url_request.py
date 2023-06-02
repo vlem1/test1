bde4177a2fa3108b339c5e50a7cc0c1ea9e973a5
vadimlemesko <lemeshko2001@mail.ru>
2023-06-02 12:56:18 +0300

    create scheduler

---
+++ scheduler/url_request.py
@@ -0,0 +1,67 @@
+import urllib.request
+from urllib.error import HTTPError
+from time import strftime, time
+import asyncio
+import httpx
+import os
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+'''
+def benchmark(func):
+    async def wrapper(*args, **kwargs):
+        start = time.time()
+        return_value = func(*args, **kwargs)
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end-start))
+        return await return_value
+    return wrapper
+
+
+@benchmark
+'''
+
+
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+
+
+async def request_url(url: str, url_list: list):
+    async with httpx.AsyncClient() as client:
+        start_ = time()
+        try:
+            response = await client.get(url)
+            print(f'{url_list.index(url) + 1} ' +
+                  url + " Code = " + str(response.status_code) + "\n-----" + str(time()-start_))
+        except HTTPError as e:
+            print(f'{url_list.index(url) + 1} ' +
+                  url + " Error occured! " + "\n-----" + str(time()-start_) + "\n" + str(e))
+
+
+async def queue():
+    tasks = []
+    url_list = ['https://www.youtube.com',
+                'https://vk.com',
+                'https://pythonru.com',
+                'https://www.speedtest.net',
+                'https://www.icloud.com',
+                'https://www.twitch.tv',
+                'https://hd.kinopoisk.ru',
+                'https://stackoverflow.com',
+                'https://www.aviasales.ru',
+                'https://hookahplace.ru',
+                ]
+    for url in url_list:
+        tasks.append(asyncio.create_task(request_url(url, url_list)))
+    await asyncio.gather(*tasks)
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(queue, 'interval', seconds=4)
+scheduler.start()
+try:
+    asyncio.get_event_loop().run_forever()
+except (KeyboardInterrupt, SystemExit):
+    pass
+
